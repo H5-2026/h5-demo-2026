15f18ae4fe338f2a15dff98f2c6ee1292a6ff97b
H5-2026 <2071006141@QQ.COM>
2026-05-04 18:47:26 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '69d5f727af4cf4b79805b0f8';
+_mrmcp['creative_id'] = '69f5889e902c486d3a00f646';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 532;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '橙衣执甲绽芳华——记灵台全国三八红旗集体环卫女工';
+_mrmcp['build_number'] = parseInt('359');
+_mrmcp['publish_time'] = 'Mon May 04 2026 17:26:21 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.com";
+})();
